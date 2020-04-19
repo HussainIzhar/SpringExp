5e5d910ce8206b1505a07c95747faed802e35e5d
Izhar Hussain <hussain786izhar@gmail.com>
2020-04-20 00:13:27 +0530

    Spring Learn

--- Spring_Exp01/Shapes/shapes/Triangle.java
+++ Spring_Exp01/Shapes/shapes/Triangle.java
@@ -2,8 +2,68 @@ package shapes;
 
 public class Triangle {
 	
+	private String type;
+	private int sides;
+	private Point PointA;
+	private Point PointB;
+	
+	
+	
+
+
+	public Triangle() {
+		super();
+	}
+
+	public Triangle(String type, int sides, Point pointA, Point pointB) {
+		super();
+		this.type = type;
+		this.sides = sides;
+		PointA = pointA;
+		PointB = pointB;
+	}
+
+	public Point getPointA() {
+		return PointA;
+	}
+
+	public void setPointA(Point pointA) {
+		PointA = pointA;
+	}
+
+	public Point getPointB() {
+		return PointB;
+	}
+
+	public void setPointB(Point pointB) {
+		PointB = pointB;
+	}
+
+	public int getSides() {
+		return sides;
+	}
+
+	public void setSides(int sides) {
+		this.sides = sides;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public void draw(){
 		System.out.println("Triangle drawn");
 	}
 
+	@Override
+	public String toString() {
+		return ("Triangle [PointA= (" + PointA.getX()+" ," +PointA.getY()+")]"
+				+"[PointB= (" + PointB.getX()+" ," +PointB.getY()+")]");
+	}
+
+	
 }
--- Spring_Exp01/src/main/java/spring/Spring_Exp01/DrawingAppln.java
+++ Spring_Exp01/src/main/java/spring/Spring_Exp01/DrawingAppln.java
@@ -5,13 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import shapes.Triangle;
 
-public class DrawingAppl {
+public class DrawingAppln {
 
-	
-	public static void main(String [] args){
+	public static void main(String[] args) {
 		ApplicationContext context =new ClassPathXmlApplicationContext("spring.xml");
 		Triangle tr= (Triangle) context.getBean("triangle");
 		tr.draw();	
-		
+		System.out.println(tr.getType());
+		System.out.println(tr.getSides());
+		System.out.println(tr.getPointA());
+		System.out.println(tr.getPointB());
+
 	}
+
 }
+
